ddbf3083e431e574d06c7510df49b5968c8d6c66
sMeatBear <fcandroidme@gmail.com>
2020-05-09 21:19:42 -0700

    Update RegexMatcher.java

--- src/main/java/pers/jianfeic/util/RegexMatcher.java
+++ src/main/java/pers/jianfeic/util/RegexMatcher.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class RegexMatcher {
 	public static String match(final String content, final String regex) {
 		String res = "";
-		Pattern pattern = Pattern.compile(regex);
+		Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
 		Matcher matcher = pattern.matcher(content);
 		// return first
 		if (matcher.find()) {
